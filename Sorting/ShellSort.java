4a31cfff9f822043520730638f7b60228d7fbdcf
Jun Wang <junw3@uw.edu>
2022-07-23 17:55:43 -0700

    implemented ShellSort

--- Sorting/ShellSort.java
+++ Sorting/ShellSort.java
@@ -2,6 +2,19 @@ package Sorting;
 
 public class ShellSort {
     public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
 
+        int N = arr.length;
+        for (int gap = N / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < N; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = temp;
+            }
+        }
     }
 }
