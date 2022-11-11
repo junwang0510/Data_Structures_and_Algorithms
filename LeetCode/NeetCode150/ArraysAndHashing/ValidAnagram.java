4684b288b1dbc7334adbb77164016e60ecbe3446
Jun Wang <junw3@uw.edu>
2022-11-11 03:39:16 -0800

    validAnagram

---
+++ LeetCode/NeetCode150/ArraysAndHashing/ValidAnagram.java
@@ -0,0 +1,29 @@
+package LeetCode.NeetCode150.ArraysAndHashing;
+
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        // Runtime: O(n)
+        // Space: O(n)
+
+        // simple check
+        if (s.length() != t.length()) return false;
+
+        // track the count of each letter in String
+        int[] letters = new int[26];
+
+        // check whether the occurrences of letters match
+        for (char c : s.toCharArray()) {
+            letters[(int) (c - 'a')]++;
+        }
+
+        for (char c : t.toCharArray()) {
+            letters[(int) (c - 'a')]--;
+        }
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
