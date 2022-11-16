da49ebd46de13c05e53e19b8f29c2a2e436f6f22
Jun Wang <junw3@uw.edu>
2022-11-16 01:19:21 -0800

    sparse vectors DP

--- LeetCode/Others/DotProduct.java
+++ LeetCode/Others/DotProduct.java
@@ -3,18 +3,33 @@ package LeetCode.Others;
 import java.util.*;
 
 public class DotProduct {
-    private int len;
-    private int[] vect;
+    // Runtime: O(n)
+    // Space: O(n)    unlikely to be n because we are processing sparse vectors
+
+    // Note:
+    // This approach is less optimal than the intuitive approach with two arrays.
+    // However, it presents a way of thinking matching the idea of sparse vectors.
+
+    private Map<Integer, Integer> map;
 
     DotProduct(int[] nums) {
-        this.len = nums.length;
-        this.vect = Arrays.copyOf(nums, len);
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (n != 0) {
+                // store indices with non-zero values
+                map.put(i, n);
+            }
+        }
     }
 
     public int dotProduct(DotProduct vec) {
         int res = 0;
-        for (int i = 0; i < this.len; i++) {
-            res += this.vect[i] * vec.vect[i];
+        for (int n : vec.map.keySet()) {
+            // only if both collections have nonzero values at index n
+            if (this.map.containsKey(n)) {
+                res += this.map.get(n) * vec.map.get(n);
+            }
         }
         return res;
     }
