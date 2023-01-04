006214a1f60ce10613023e832028842bb58aaf87
Jun Wang <junw3@uw.edu>
2023-01-04 00:05:30 -0800

    added comments

--- LeetCode/NeetCode150/BitManipulation/HammingWeight.java
+++ LeetCode/NeetCode150/BitManipulation/HammingWeight.java
@@ -2,6 +2,9 @@ package LeetCode.NeetCode150.BitManipulation;
 
 public class HammingWeight {
     public int hammingWeight(int n) {
+        // Time: O(1)
+        // Space: O(1)
+
         int ct = 0;
         int mask = 1;
         for (int i = 0; i < 32; i++) {
